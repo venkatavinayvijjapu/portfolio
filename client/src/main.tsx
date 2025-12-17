748af541043b39f186e980d4d5c76c3c8b3978ab
Venkata Vinay Vijjapu <102150068+venkatavinayvijjapu@users.noreply.github.com>
2025-12-17 15:14:32 +0530

    Update React root rendering method
    Refactor React root rendering to use ReactDOM and StrictMode.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,15 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
+// import { createRoot } from "react-dom/client";
+// import App from "./App";
+// import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+// createRoot(document.getElementById("root")!).render(<App />);
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
